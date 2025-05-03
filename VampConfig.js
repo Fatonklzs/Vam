beb9cae76c8e61b22996c5e9ca8687c46a6b4996
Faton Klzs <fatonklzs@gmail.com>
2025-05-03 12:49:55 +0700

    first commit

---
+++ VampConfig.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [5810676711], // ganti jadi id mu
+    botToken: "7558367125:AAE_zeRFQOK2oBqoWhNM3m3IGW69hzD0XqA", //isi make token bot mu
+}
+
+module.exports = global;
